5f8643f3a7c858cd366995db4a7fbdb37b1800ba
albertoribeiro <alberto.r.flavio@gmail.com>
2020-07-11 14:36:07 -0300

    Definiçaõ do recurso de categorias

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Route::resource(name:'Categories', controller:'Api\CategoryController');
+Route::resource('Categories', 'Api\CategoryController',['except'=> ['create','edit']]);
